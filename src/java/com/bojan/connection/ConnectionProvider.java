51366c6937d168275346a2f69df084da8160d5a5
bojangabric <bojangabric1997@gmail.com>
2018-11-27 16:55:55 +0100

    Add connection to db

---
+++ src/java/com/bojan/connection/ConnectionProvider.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bojan.connection;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author bojan
+ */
+public class ConnectionProvider {
+
+    public static Connection getCon() {
+        String CONNECTION_URL = "jdbc:mysql://localhost:3306/ipa_projekat";
+        String USERNAME = "root";
+        String PASSWORD = "123";
+
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return con;
+    }
+
+}
